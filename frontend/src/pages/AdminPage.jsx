77d1af4a7ddfbd84476feb762ad4e1e2cc46cfc6
tomasmatos2003 <tomas.matos@ua.pt>
2024-05-07 22:38:43 +0100

    SCRUM-10 admin checkin done

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,11 +1,75 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Footer from "../components/Footer";
 import logo from "../assets/logo.png";
+import Footer from "../components/Footer";
 
 function AdminPage() {
   const [flightsInfo, setFlightsInfo] = useState(true);
   const [checkin, setCheckin] = useState(false);
+  const [passengerInfo, setPassengerInfo] = useState({
+    name: '',
+    passengerBI: '',
+    checkInTime: '',
+    numberOfBags: 0,
+    bagWeights: []
+  });
+
+
+  const handleBagWeightChange = (index, value) => {
+    if (isNaN(value)) {
+      value = 0;
+    }
+    setPassengerInfo(prevState => {
+      const updatedBagWeights = [...prevState.bagWeights];
+      updatedBagWeights[index] = value;
+      return {
+        ...prevState,
+        bagWeights: updatedBagWeights
+      };
+    });
+  };
+  
+  const handleSubmitCheckIn = (event) => {
+    event.preventDefault();
+   
+    let hasErrors = false;
+
+    
+    if (passengerInfo.passengerBI.toString().length != 9) {
+      alert('BI number must be have 9 digits.');
+      hasErrors = true;
+    }
+
+    if (passengerInfo.numberOfBags > 6) {
+      alert('Number of bags cannot be negative.');
+      hasErrors = true;
+    }
+
+    if (passengerInfo.bagWeights.some(weight => weight > 40)) {
+      alert('Bag weight cannot exceed 40 kg.');
+      hasErrors = true;
+    }
+
+    if (passengerInfo.name === '' || passengerInfo.passengerBI === '' || passengerInfo.checkInTime === '' ) {
+      alert('Please fill in all fields.');
+      hasErrors = true;
+    }
+    
+    if (!hasErrors) {
+      alert('Check-in successful!');
+      console.log('Passenger Info:', passengerInfo);
+      event.target.reset();
+      setPassengerInfo({
+        name: '',
+        passengerBI: '',
+        checkInTime: '',
+        numberOfBags: 0,
+        bagWeights: []
+      });
+    }    
+  };
+
+
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -117,7 +181,94 @@ function AdminPage() {
             </div>
           )}
 
-          {checkin && <div>checkin</div>}
+          {checkin && (
+            
+            <div>
+              <div className="mb-10">
+                <h1 className="text-4xl font-bold">Check-In</h1>
+              </div>
+
+              <form onSubmit={handleSubmitCheckIn} className="space-y-4">
+                <div className="flex flex-col">
+                  <label htmlFor="name" className="text-lg font-medium">Name:</label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={passengerInfo.name}
+                    onChange={(e) => setPassengerInfo(prevState => ({ ...prevState, name: e.target.value }))}
+                    required
+                    className="border border-gray-300 rounded-md px-4 py-2"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="passengerBI" className="text-lg font-medium">BI:</label>
+                  <input
+                    type="text"
+                    id="passengerBI"
+                    name="passengerBI"
+                    value={passengerInfo.passengerBI}
+                    onChange={(e) => setPassengerInfo(prevState => ({ ...prevState, passengerBI: e.target.value }))}
+                    required
+                    className="border border-gray-300 rounded-md px-4 py-2"
+                  />
+                  {(passengerInfo.passengerBI.toString().length > 9) && (
+                    <p className="text-red-600">BI number have 9 digits.</p>
+                  )}
+                  {(passengerInfo.passengerBI.toString().length < 9 && passengerInfo.passengerBI.toString().length > 0 ) && (
+                    <p className="text-red-600">Add more {9-passengerInfo.passengerBI.toString().length} digit(s)</p>
+                  )}
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="checkInTime" className="text-lg font-medium">Check-in Time:</label>
+                  <input
+                    type="datetime-local"
+                    id="checkInTime"
+                    name="checkInTime"
+                    value={passengerInfo.checkInTime}
+                    onChange={(e) => setPassengerInfo(prevState => ({ ...prevState, checkInTime: e.target.value }))}
+                    required
+                    className="border border-gray-300 rounded-md px-4 py-2"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="numberOfBags" className="text-lg font-medium">Number of Bags:</label>
+                  <input
+                    type="number"
+                    id="numberOfBags"
+                    value={passengerInfo.numberOfBags}
+                    onChange={(e) => setPassengerInfo(prevState => ({ ...prevState, numberOfBags: parseInt(e.target.value) }))}
+                    min={0}
+                    required
+                    className="border border-gray-300 rounded-md px-4 py-2"
+                  />
+                  {passengerInfo.numberOfBags > 6 && (
+                    <p className="text-red-600">Number of bags cannot be greater than 6.</p>
+                  )}
+                </div>
+                {Array.from({ length: passengerInfo.numberOfBags }, (_, index) => (
+                  <div key={index} className="flex flex-col">
+                    <label htmlFor={`bagWeight${index + 1}`} className="text-lg font-medium">{`Bag ${index + 1} Weight (kg):`}</label>
+                    <input
+                      type="number"
+                      id={`bagWeight${index + 1}`}
+                      value={passengerInfo.bagWeights[index]}
+                      onChange={(e) => handleBagWeightChange(index, parseFloat(e.target.value))}
+                      min={0}
+                      required
+                      className="border border-gray-300 rounded-md px-4 py-2"
+                    />
+                    {passengerInfo.bagWeights[index] > 40 && (
+                      <p className="text-red-600">Bag weight cannot exceed 40 kg.</p>
+                    )}
+                  </div>
+                ))}
+                <button type="submit" className="bg-green-600 text-white rounded-md px-4 py-2 hover:bg-green-700">Check In</button>
+              </form>
+            </div>
+
+      
+          )}
         </div>
       </div>
 
