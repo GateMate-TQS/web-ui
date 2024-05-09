32bc4cadf51a454ca05f1de2eddbdab5f63c5bc9
Diogo <almeidadiogo03@gmail.com>
2024-05-09 17:04:30 +0100

    SCRUM-11 change background

--- frontend/src/pages/Checkin.jsx
+++ frontend/src/pages/Checkin.jsx
@@ -7,6 +7,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import airplaneIcon from "../assets/plane.webp";
 import "leaflet-rotatedmarker";
+import image1 from "../assets/allflights/1.jpeg";
 
 function FlightTracker() {
   const [flights, setFlights] = useState([]);
@@ -97,11 +98,12 @@ function FlightTracker() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="mb-10">
+      <div className="">
         <Navbar />
       </div>
-      <div className="flex flex-row z-0 justify-center items-center">
-        <div className="w-1/3 p-5 mt-7 flex flex-col">
+      <div className="flex-1 " style={{backgroundImage: `url(${image1})`,backgroundSize: 'cover', backgroundPosition: 'center'}}>
+      <div className="flex flex-col z-0 justify-center items-center mt-20">
+        <div className="w-1/3 p-5 mt-9 flex flex-col mb-20" style={{backgroundColor:"white", borderRadius:"10px"}}>
           <div>
             <button className="text-blue-400" onClick={() => resetFilters()}>
               <u>Reset all</u>
@@ -116,25 +118,15 @@ function FlightTracker() {
               <div className="font-bold">Booking Reference Number</div>
               <hr />
               <div className="mt-3">
-                <select
-                  className="pl-2 appearance-none bg-gray-100 text-zinc-600 w-full h-10 text-xl font-normal outline-none inline-fle"
+                <input
+                  type="text"
+                  className="pl-2 appearance-none bg-gray-100 text-zinc-600 w-full h-10 text-xl font-normal outline-none"
                   value={filter.company}
                   onChange={(e) =>
                     setFilter({ ...filter, company: e.target.value })
                   }
-                >
-                  <option value="" selected>
-                  Booking Reference Number
-                  </option>
-                  {flights &&
-                    [
-                      ...new Set(flights.map((flight) => flight.airlineName)),
-                    ].map((uniqueName) => (
-                      <option key={uniqueName} value={uniqueName}>
-                        {uniqueName}
-                      </option>
-                    ))}
-                </select>
+                  placeholder="Enter Booking Reference Number"
+                />
               </div>
             </div>
           </div>
@@ -143,32 +135,20 @@ function FlightTracker() {
               <div className="font-bold">Last Name</div>
               <hr />
               <div className="mt-3">
-                <select
-                  className="pl-2 appearance-none bg-gray-100 text-zinc-600 w-full h-10 text-xl font-normal outline-none inline-flex"
+                <input
+                  type="text"
+                  className="pl-2 appearance-none bg-gray-100 text-zinc-600 w-full h-10 text-xl font-normal outline-none"
                   value={filter.from}
                   onChange={(e) =>
                     setFilter({ ...filter, from: e.target.value })
                   }
-                >
-                  <option value="" selected>
-                    Last Name
-                  </option>
-                  {flights &&
-                    [
-                      ...new Set(
-                        flights.map((flight) => flight.departure.iata)
-                      ),
-                    ].map((uniqueIata) => (
-                      <option key={uniqueIata} value={uniqueIata}>
-                        {uniqueIata}
-                      </option>
-                    ))}
-                </select>
+                  placeholder="Enter Last Name"
+                />
               </div>
             </div>
           </div>
 
-          <div className="py-5">
+          <div className="py-13">
             <button
               className="w-full bg-blue-700 rounded justify-center items-center inline-flex px-12 py-2 text-center text-white text-base font-bold leading-normal"
               onClick={() => handleSearch()}
@@ -178,11 +158,13 @@ function FlightTracker() {
           </div>
         </div>
       </div>
-      <div className="mt-10">
+      </div>
+      <div className="mt-auto">
         <Footer />
       </div>
     </div>
   );
 }
+
 
 export default FlightTracker;
